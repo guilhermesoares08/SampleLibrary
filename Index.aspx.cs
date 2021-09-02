e50eb1e77351e9754d93d9fe54b173a335696ce7
Guilherme Soares <guilhermesoares1996@hotmail.com>
2021-09-01 23:58:12 -0300

    adjust the codebehind to start main page

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -23,27 +23,6 @@ namespace LibrarySystem
         }
         protected void Page_Load(object sender, EventArgs e)
         {
-            int q = 0;
-            decimal p1 = 4M;
-            decimal p2 = 5M;
-
-            List<Domain.Book> lst = new List<Domain.Book>();
-            lst.Add(new Domain.Book() { Id = 1, Description = "l1" });
-            lst.Add(new Domain.Book() { Id = 2, Description = "l2" });
-            lst.Add(new Domain.Book() { Id = 3, Description = "l3" });
-
-            foreach (Domain.Book joirAbi in lst)
-            {
-                if (joirAbi.Description.Equals("l2"))
-                {
-                    Console.WriteLine("digite o nome novo");
-                    joirAbi.Description = Console.ReadLine();
-
-                }
-            }
-
-
-            
             //lstBooks = BookService.GetBookByFilter(txtSearch.Value, cbFilterGender.Value);
             if (!IsPostBack)
             {
